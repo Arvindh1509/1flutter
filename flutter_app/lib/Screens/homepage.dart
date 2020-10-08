a37065267ff09598baa64b3a93d6a90940ab6956
arvindh <rockstararvindh@gmail.com>
2020-10-08 13:59:28 +0530

    Update homepage.dart

--- flutter_app/lib/Screens/homepage.dart
+++ flutter_app/lib/Screens/homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Screens/about.dart';
 void main() =>runApp(MaterialApp(
 home: JsonPage(),
 ));
@@ -20,22 +21,67 @@ class _JsonPageState extends State<JsonPage> {
           return ListView.builder(
 
             itemBuilder: (BuildContext context, int index){
-              return Column(
-                children: [
-                  ListTile(
-                    title: Text(showData[index]['Adobe XD event']),
+              return ListTile (
+        
+               title:Text(showData[index]['Adobe XD event']),
                     focusColor: Colors.lightGreenAccent,
-                  ),
-                ],
-              );
-            },
+                  );
+                
+                },
             itemCount: showData.length,
           );
         }, future: DefaultAssetBundle.of(context).loadString("assets/dsc.json"),
 
         ),
       ),
+ drawer: Drawer(
+         child: Column(
+           children: <Widget>[
+            Container(
 
+              width: double.infinity,
+              padding: EdgeInsets.all(80.0),
+              color: Colors.amber,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                  child: Text('DSC demo app', style: TextStyle(
+                fontStyle: FontStyle.normal,
+              color: Colors.black,),),)
+
+            ),
+             ListTile(
+               leading: Icon(Icons.home),
+               title: Text(
+                 'HOME',
+                 style: TextStyle(
+                   fontStyle: FontStyle.normal,
+                   fontSize: 18,
+                 ),
+               ),
+               onTap:(){
+                 Navigator.push(context,MaterialPageRoute(
+                   builder: (context) => JsonPage()),
+                 );
+               },
+             ),
+           ListTile(
+            leading: Icon(Icons.info),
+            title: Text(
+                  'About us',
+                      style: TextStyle(
+                fontStyle: FontStyle.normal,
+              fontSize: 18,
+        ),
+      ),
+      onTap: (){
+              Navigator.push(context,MaterialPageRoute(
+                  builder: (context)=>MyAboutPage(),
+              ));
+      }
+           ),
+           ]
+         ),
+       ),
     );
   }
 }
